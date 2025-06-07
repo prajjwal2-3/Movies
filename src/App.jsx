83f0d1907a42ad9f2028829fcaa6a8271c0f3415
Prajjwal sharma <prajjwalbh25@gmail.com>
2025-06-07 16:33:51 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "./App.css";
-
+import React from "react";
+import "./index.css";
 function App() {
   return (
-    <div className="App">
+    <div className="text-red-500 text-3xl font-bold underline">
       Namaste Everyone
-      </div>
-  )
+    </div>
+  );
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+import App from './App'
+import React from 'react'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
